aff9d7b41d50098161878959a233d74397d16b3f
bugsommelier <adamwdd@gmail.com>
2023-12-03 17:50:46 -0600

    Formatted for readability

--- src/picoSteg.py
+++ src/picoSteg.py
@@ -7,6 +7,7 @@ import cv2
 # Declare global variables
 global image_path, target_save_path, message_entry, original_image_tk
 
+
 def load_image():
     global image_path, target_save_path, original_image_tk
     # Get the image path
@@ -37,6 +38,7 @@ def load_image():
         decoded_image_label.configure(image=None)  # Clear the displayed image
         results_label.configure(text="")
 
+
 def encode_and_save_message():
     global message_entry
     # Get the message to hide
@@ -62,14 +64,17 @@ def encode_and_save_message():
 
     # Display a success message
     results_label.configure(text="Encoded and saved to:")
-    results_label.configure(text=results_label['text'] + " " + os.path.join(target_save_path, filename))
+    results_label.configure(
+        text=results_label["text"] + " " + os.path.join(target_save_path, filename)
+    )
+
 
 def encode_message(image_path, message, target_save_path, filename):
     # Open the image using OpenCV
     original_image = cv2.imread(image_path)
 
     # Convert the message to a binary string
-    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    binary_message = "".join(format(ord(char), "08b") for char in message)
 
     # Flatten the image
     flat_image = original_image.flatten()
@@ -84,6 +89,7 @@ def encode_message(image_path, message, target_save_path, filename):
     # Save the encoded image to the specified path
     encoded_image_path = os.path.join(target_save_path, filename)
     cv2.imwrite(encoded_image_path, encoded_image)
+
 
 def load_decode():
     global decoded_image_label, original_image_tk
@@ -113,6 +119,7 @@ def load_decode():
     # Clear the message entry
     message_entry.delete(0, tk.END)
 
+
 def decode_message(encoded_image_path):
     # Open the encoded image using OpenCV
     encoded_image = cv2.imread(encoded_image_path)
@@ -121,20 +128,26 @@ def decode_message(encoded_image_path):
     flat_encoded_image = encoded_image.flatten()
 
     # Extract the LSBs to retrieve the binary message
-    binary_message = ''.join(str(pixel & 1) for pixel in flat_encoded_image)
+    binary_message = "".join(str(pixel & 1) for pixel in flat_encoded_image)
 
     # Convert the binary message to ASCII
-    hidden_message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
+    hidden_message = "".join(
+        chr(int(binary_message[i : i + 8], 2)) for i in range(0, len(binary_message), 8)
+    )
 
     # Reshape the flat image back to its original shape
     decoded_image = flat_encoded_image.reshape(encoded_image.shape)
 
     return hidden_message, decoded_image
 
+
 def update_results(hidden_message):
     # Update the decoded message label with the decoded message
     decoded_message_label.configure(text="Decoded Message:")
-    decoded_message_label.configure(text=decoded_message_label['text'] + "\n" + hidden_message)
+    decoded_message_label.configure(
+        text=decoded_message_label["text"] + "\n" + hidden_message
+    )
+
 
 # Create the main window and its components
 window = tk.Tk()
@@ -143,8 +156,7 @@ window.title("picoSteg")
 image_label = tk.Label(window)
 image_label.pack()
 
-load_image_button = tk.Button(
-    window, text="Browse for an image", command=load_image)
+load_image_button = tk.Button(window, text="Browse for an image", command=load_image)
 load_image_button.pack()
 
 message_label = tk.Label(window, text="Message:")
@@ -153,12 +165,10 @@ message_label.pack()
 message_entry = tk.Entry(window, width=40)
 message_entry.pack()
 
-encode_message_button = tk.Button(
-    window, text="Save", command=encode_and_save_message)
+encode_message_button = tk.Button(window, text="Save", command=encode_and_save_message)
 encode_message_button.pack()
 
-decode_message_button = tk.Button(
-    window, text="Load for decode", command=load_decode)
+decode_message_button = tk.Button(window, text="Load for decode", command=load_decode)
 decode_message_button.pack()
 
 decoded_message_label = tk.Label(window, text="", wraplength=300)
